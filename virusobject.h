87455aed4ab634ae6edfee766ac3ad357e62b69c
OmarJaroudi <30807936+OmarJaroudi@users.noreply.github.com>
2020-11-05 13:20:44 +0200

    Create virusobject.h

---
+++ virusobject.h
@@ -0,0 +1,32 @@
+#ifndef VIRUSOBJECT_H
+#define VIRUSOBJECT_H
+#include "clickablelabel.h"
+#include <QMediaPlayer>
+#include <QMediaPlaylist>
+#include <QTimer>
+using namespace std;
+class VirusObject : public ClickableLabel
+{
+    Q_OBJECT
+public:
+    enum Color {GREEN,YELLOW,BLUE};
+    QString getColor();
+    int getScore();
+    int getSize();
+    VirusObject();
+    QMediaPlayer *smash_sound;
+    QTimer * expiry_timer;
+    VirusObject(VirusObject::Color c,double speed);
+    static bool recent_miss;
+    static int total_misses;
+    static vector<int> missed_shots;
+    double rolling_speed;
+
+private:
+    Color color;
+    int score;
+    int size;
+public slots: void Expire();
+};
+
+#endif // VIRUSOBJECT_H
